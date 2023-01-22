47103b3d47a26c48dcf54325ceab077fd0abd757
Eld0d0 <nils.gill.arion@gmail.com>
2023-01-22 21:09:48 +0100

    Modification pour réellement intégrer bootstrap

--- migrations/Version20230122173710.php
+++ migrations/Version20230122173710.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230118130210 extends AbstractMigration
+final class Version20230122173710 extends AbstractMigration
 {
     public function getDescription(): string
     {
